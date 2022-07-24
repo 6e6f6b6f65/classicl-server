9ce2ed01f246f07eba2bb8d60371ff49b06e6f92
nokoe <10518515-nokoe@users.noreply.gitlab.com>
2022-07-25 00:18:50 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,186 @@
+use classicl::{client, server::*, ClientController};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use crate::terrain::{Terrain, TerrainNoise};
+
+mod terrain;
+
+const PLAYER_HEIGHT: i16 = 51 * 2;
+
+#[tokio::main]
+async fn main() {
+    let mut server = classicl::Server::new("0.0.0.0:25565").await.unwrap();
+
+    let pdb: Arc<Mutex<HashMap<i8, Player>>> = Arc::new(Mutex::new(HashMap::new()));
+    let pq = Arc::new(Mutex::new(HashMap::new()));
+
+    let terrain = Arc::new(Mutex::new(Terrain::new((100, 32, 100))));
+
+    let players = pq.clone();
+    let map = terrain.clone();
+    let spawn_point = (
+        to_fixed_point(10.0),
+        to_fixed_point(TerrainNoise::new().height(10, 10)) + PLAYER_HEIGHT,
+        to_fixed_point(10.0),
+    );
+    server
+        .on_client_connected(move |_s, id, c| {
+            let mut players = players.lock().unwrap();
+            players.insert(id, c.clone());
+
+            c.write_packet(&LevelInitialize {}).unwrap();
+            for i in &map.lock().unwrap().to_chunks() {
+                c.write_packet(i).unwrap();
+            }
+
+            c.write_packet(&LevelFinalize {
+                x_size: 100,
+                y_size: 32,
+                z_size: 100,
+            })
+            .unwrap();
+
+            c.write_packet(&Message {
+                player_id: 0,
+                message: "Welcome".to_string(),
+            })
+            .unwrap();
+        })
+        .await;
+
+    let players = pdb.clone();
+    let queue = pq.clone();
+    server
+        .on_player_identification(move |id, p| {
+            let mut players = players.lock().unwrap();
+            if let Some(c) = queue.lock().unwrap().remove(&id) {
+                let player = Player {
+                    c: c.clone(),
+                    player_name: p.username.to_string(),
+                    x: spawn_point.0,
+                    y: spawn_point.1,
+                    z: spawn_point.2,
+                    yaw: 0,
+                    pitch: 0,
+                };
+
+                for (pid, p) in players.iter() {
+                    p.c.write_packet(&player.to_spawn(id)).unwrap();
+                    c.write_packet(&p.to_spawn(*pid)).unwrap();
+                }
+                c.write_packet(&player.to_spawn(-1)).unwrap();
+                players.insert(id, player);
+            }
+        })
+        .await;
+
+    let players = pdb.clone();
+    let map = terrain.clone();
+    server
+        .on_set_block(move |_, p| {
+            let block_type = if p.mode == 0x00 {
+                terrain::Blocks::Air as u8
+            } else {
+                p.block_type
+            };
+            map.lock().unwrap().set_block(p.x, p.y, p.z, block_type);
+            for (_, player) in players.lock().unwrap().iter_mut() {
+                player
+                    .c
+                    .write_packet(&SetBlock {
+                        x: p.x,
+                        y: p.y,
+                        z: p.z,
+                        block_type,
+                    })
+                    .unwrap();
+            }
+        })
+        .await;
+
+    let players = pdb.clone();
+    server
+        .on_position_orientation(move |id, p| {
+            let mut players = players.lock().unwrap();
+            let mut mplayer = None;
+            if let Some(player) = players.get_mut(&id) {
+                player.set_pos_ori(p);
+                mplayer = Some(player.clone())
+            }
+            if let Some(player) = mplayer {
+                for (i, p) in players.iter_mut() {
+                    if *i != id {
+                        p.c.write_packet(&player.to_pos_ori_upd(id)).unwrap();
+                    }
+                }
+            }
+        })
+        .await;
+
+    let players = pdb.clone();
+    let queue = pq.clone();
+
+    server
+        .on_client_disconnected(move |id| {
+            let _ = players.lock().unwrap().remove(&id);
+            let _ = queue.lock().unwrap().remove(&id);
+            for (_, p) in players.lock().unwrap().iter_mut() {
+                p.c.write_packet(&DespawnPlayer { player_id: id }).unwrap();
+            }
+        })
+        .await;
+    tokio::join!(server.run());
+}
+
+#[derive(Clone)]
+pub struct Player {
+    pub c: ClientController,
+    pub player_name: String,
+    pub x: i16,
+    pub y: i16,
+    pub z: i16,
+    pub yaw: u8,
+    pub pitch: u8,
+}
+
+impl Player {
+    pub fn to_spawn(&self, id: i8) -> SpawnPlayer {
+        SpawnPlayer {
+            player_id: id,
+            player_name: self.player_name.to_string(),
+            x: self.x,
+            y: self.y,
+            z: self.z,
+            yaw: self.yaw,
+            pitch: self.pitch,
+        }
+    }
+
+    // ClassiCube does not understand PositionOrientationUpdate
+    // maybe this is not meant to be used
+    pub fn to_pos_ori_upd(&self, id: i8) -> client::PositionOrientation {
+        client::PositionOrientation {
+            player_id: id.to_be_bytes()[0],
+            x: self.x,
+            y: self.y,
+            z: self.z,
+            yaw: self.yaw,
+            pitch: self.pitch,
+        }
+    }
+
+    pub fn set_pos_ori(&mut self, p: &client::PositionOrientation) {
+        self.x = p.x;
+        self.y = p.y;
+        self.z = p.z;
+        self.yaw = p.yaw;
+        self.pitch = p.pitch;
+    }
+}
+
+fn to_fixed_point(v: f64) -> i16 {
+    (v * 32.0).round() as i16
+}
---
+++ src/terrain.rs
@@ -0,0 +1,180 @@
+pub enum Blocks {
+    Air = 0,
+    Stone = 1,
+    Grass = 2,
+    /*
+    Dirt = 3,
+    Cobblestone = 4,
+    Wood = 5,
+    Sapling = 6,
+    Bedrock = 7,
+    Water = 8,
+    StillWater = 9,
+    Lava = 10,
+    StillLava = 11,
+    Sand = 12,
+    Gravel = 13,
+    GoldOre = 14,
+    IronOre = 15,
+    CoalOre = 16,
+    Log = 17,
+    Leaves = 18,
+    Sponge = 19,
+    Glass = 20,
+    Red = 21,
+    Orange = 22,
+    Yellow = 23,
+    Lime = 24,
+    Green = 25,
+    Teal = 26,
+    Aqua = 27,
+    Cyan = 28,
+    Blue = 29,
+    Indigo = 30,
+    Violet = 31,
+    Magenta = 32,
+    Pink = 33,
+    Black = 34,
+    Gray = 35,
+    White = 36,
+    Dandelion = 37,
+    Rose = 38,
+    BrownMushroom = 39,
+    RedMushroom = 40,
+    Gold = 41,
+    Iron = 42,
+    DoubleSlab = 43,
+    Slab = 44,
+    Brick = 45,
+    TNT = 46,
+    Bookshelf = 47,
+    MossyRocks = 48,
+    Obsidian = 49,
+    CobblestoneSlab = 50,
+    Rope = 51,
+    Sandstone = 52,
+    Snow = 53,
+    Fire = 54,
+    LightPink = 55,
+    ForestGreen = 56,
+    Brown = 57,
+    DeepBlue = 58,
+    Turquoise = 59,
+    Ice = 60,
+    CeramicTile = 61,
+    Magma = 62,
+    Pillar = 63,
+    Crate = 64,
+    StoneBrick = 65,
+    */
+}
+
+use std::io::Write;
+
+use classicl::server::LevelDataChunk;
+use libflate::gzip::Encoder;
+use noise::{Add, Constant, NoiseFn, ScalePoint, SuperSimplex};
+
+pub struct TerrainNoise {
+    height: SuperSimplex,
+    caves: SuperSimplex,
+}
+
+impl TerrainNoise {
+    pub fn new() -> Self {
+        Self {
+            height: SuperSimplex::new(),
+            caves: SuperSimplex::new(),
+        }
+    }
+
+    pub fn height(&self, x: i16, y: i16) -> f64 {
+        let h = Constant::new(20.0);
+        let added = Add::new(&self.height, &h);
+        let mut noise = ScalePoint::new(added);
+        noise.x_scale = 0.1;
+        noise.y_scale = 0.1;
+        noise.z_scale = 0.1;
+        noise.get([x as f64, y as f64])
+    }
+
+    pub fn cave(&self, x: i16, y: i16, z: i16) -> f64 {
+        let mut noise = ScalePoint::new(self.caves);
+        noise.x_scale = 0.1;
+        noise.y_scale = 0.1;
+        noise.z_scale = 0.1;
+        noise.get([x as f64, y as f64, z as f64])
+    }
+}
+
+pub struct Terrain {
+    size: (i16, i16, i16),
+    inner: Vec<u8>,
+}
+
+impl Terrain {
+    pub fn new(size: (i16, i16, i16)) -> Self {
+        Self {
+            size,
+            inner: Self::generate(size),
+        }
+    }
+
+    fn generate(size: (i16, i16, i16)) -> Vec<u8> {
+        let noise = TerrainNoise::new();
+        let (x, y, z) = size;
+        let mut buf = vec![];
+        for y in 0..y {
+            for x in 0..x {
+                for z in 0..z {
+                    let h = noise.height(x, z);
+                    if y as f64 > h {
+                        buf.push(Blocks::Air as u8);
+                    } else {
+                        if noise.cave(x, y, z) > 0.05 {
+                            if h.floor() as i16 - y > 5 {
+                                buf.push(Blocks::Stone as u8);
+                            } else {
+                                buf.push(Blocks::Grass as u8)
+                            }
+                        } else {
+                            buf.push(Blocks::Air as u8);
+                        }
+                    }
+                }
+            }
+        }
+        buf
+    }
+
+    pub fn to_chunks(&self) -> Vec<LevelDataChunk> {
+        let mut buf = vec![];
+        let mut map = Encoder::new(&mut buf).unwrap();
+        let size: u32 = self.size.0 as u32 * self.size.1 as u32 * self.size.2 as u32;
+        let mut raw = Vec::from(size.to_be_bytes());
+        raw.append(&mut self.inner.clone());
+        map.write_all(&raw).unwrap();
+        let data = map.finish().unwrap().0;
+        let mut bytes_sent = 0;
+        let total = data.len();
+        data.chunks(1024)
+            .map(|x| {
+                bytes_sent += x.len();
+                let percent_complete = ((bytes_sent as f32 / total as f32) * 100.0).floor() as u8;
+                LevelDataChunk {
+                    chunk_length: x.len() as i16,
+                    chunk_data: Vec::from(x),
+                    percent_complete,
+                }
+            })
+            .collect()
+    }
+
+    pub fn set_block(&mut self, x: i16, y: i16, z: i16, t: u8) {
+        let (x_size, _, z_size) = self.size;
+        let index = x as usize + x_size as usize * (z as usize + z_size as usize * y as usize);
+        if let Some(v) = self.inner.get_mut(index) {
+            *v = t;
+        }
+    }
+}
